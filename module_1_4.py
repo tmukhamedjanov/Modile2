66e8b33322450ca82b615bc2343d9af486e0a490
tmukhamedjanov <t.mukhamedjanov@mail.ru>
2024-10-02 01:43:20 +0300

    2

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+count_hw = 12
+count_hour = 1.5
+name_of_course = " Python"
+time = count_hour/count_hw
+print("Курс: ", name_of_course, ",", "всего задач: ", count_hw, ",", "затраченное время: ", count_hour, "," , "среднее время выполнения ", time, "часа")

f8d8159ce662e5f9f1ecd640e8518930f1ff72c6
tmukhamedjanov <t.mukhamedjanov@mail.ru>
2024-10-03 15:25:38 +0300

    3

---
+++ module_2_3.py
@@ -0,0 +1,8 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+index = 0
+while my_list[index] >= 0:
+    print(my_list[index])
+    index = index + 1
+    if index == len(my_list) :
+       break
